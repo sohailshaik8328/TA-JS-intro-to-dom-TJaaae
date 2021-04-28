62ef9cbd551a0c4373c137f17fb2c814913d4356
sohail shaik <sohailshaik765@gmail.com>
2021-04-28 07:04:30 +0530

    manipulate DOM block-4

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -7,6 +7,14 @@ default value to be "text" and return the input element inside label. (create it
 
 // Your code goes here
 
+
+function createInputElm(label, input){
+ label = document.createElement('label')
+ input = document.createElement('input')
+input.type = 'text'
+return label.append(input)
+}
+
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -22,6 +30,13 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
+function createList(arr){
+  let ul = document.createElement('ul')
+  let li = document.createElement('li')
+  li.append(arr)
+  ul.append(li)
+
+}
 
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
